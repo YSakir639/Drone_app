32ab5e648f3ae94e7bc8fcf6a11e37f3a3ed20e9
YSakir639 <103224018+YSakir639@users.noreply.github.com>
2022-08-08 14:18:15 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,1 +1,21 @@
+from djitellopy import Tello
+import cv2
+
+tello = Tello()
+tello.connect()
+
+tello.streamon()
+
+width = 1000
+height = 650
+
+while (True):
+    frame_read = tello.get_frame_read()
+    frame = frame_read.frame
+    img = cv2.resize(frame,(width,height))
+
+    cv2.imshow("Stream",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
 
